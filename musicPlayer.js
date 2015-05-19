26ed7bd403067d5e4c6626a4a0e4b24076c9f29a
Pamela Whisenhunt <pamela.whise@gmail.com>
2015-05-19 16:35:21 -0600

    finishing stretch goals

--- musicPlayer.js
+++ musicPlayer.js
@@ -2,8 +2,6 @@ var play = document.getElementsByClassName('fa-play');
 var title = document.getElementById('song-title');
 var pause = document.getElementsByClassName('fa-pause');
 
-
-
 var playOrStopAudio = function() {
   for(var i = 0; i <= play.length-1; i++ ) {
     play[i].addEventListener('click', function(e) {
@@ -23,27 +21,35 @@ var pauseAudio = function() {
   }
 };
 
+
 var togglePlay = function(that) {
   var audioFile;
   if(that.classList.contains('fa-play')) {
-    that.classList.remove('fa-play');
-    that.classList.add('fa-stop');
     audioFile = that.parentNode.children[2];
     playSong(audioFile);
+
+    audioFile.addEventListener('ended', function() {
+      audioFile.currentTime = 0;
+      that.classList.remove('fa-stop');
+      that.classList.add('fa-play');
+      var nextSong = that.parentNode.nextSibling.nextSibling.childNodes[5]
+      playSong(nextSong);
+    });
   }
   else {
-    that.classList.remove('fa-stop');
-    that.classList.add('fa-play');
     audioFile = that.parentNode.children[2];
-    populateTitle(audioFile);
     stopSong(audioFile);
   }
 };
 
 var playSong = function(audioFile) {
+  var icon = audioFile.parentNode.children[1];
+  icon.classList.remove('fa-play');
+  icon.classList.add('fa-stop');
   populateTitle(audioFile);
   audioFile.play();
 };
+
 
 var pauseSong = function(audioFile, icon) {
   title.innerHTML = 'Paused: <em>' + audioFile.dataset.title + '</em>';
@@ -54,14 +60,17 @@ var pauseSong = function(audioFile, icon) {
 };
 
 var stopSong = function(audioFile) {
+  var icon = audioFile.parentNode.children[1];
+  icon.classList.remove('fa-stop');
+  icon.classList.add('fa-play');
   audioFile.currentTime = 0;
   audioFile.pause();
+  title.innerHTML = 'Select a song!';
 };
 
 var populateTitle = function(audioFile) {
   title.innerHTML = 'Now Playing: <em>' + audioFile.dataset.title + '</em>';
 };
-
 
 playOrStopAudio();
 pauseAudio();
