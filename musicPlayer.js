ece4b485a0064c181071127812b4efaf58b16da5
Pamela Whisenhunt <pamela.whise@gmail.com>
2015-05-19 11:52:33 -0600

    added difference between play, stop, and pause

--- musicPlayer.js
+++ musicPlayer.js
@@ -1,11 +1,24 @@
 var play = document.getElementsByClassName('fa-play');
 var title = document.getElementById('song-title');
+var pause = document.getElementsByClassName('fa-pause');
 
-var playOrStop = function() {
+
+
+var playOrStopAudio = function() {
   for(var i = 0; i <= play.length-1; i++ ) {
     play[i].addEventListener('click', function(e) {
       var that = this;
       togglePlay(that);
+    });
+  }
+};
+
+var pauseAudio = function() {
+  for(var i = 0; i <= pause.length-1; i++) {
+    pause[i].addEventListener('click', function(e) {
+      var audioFile = this.parentNode.children[2];
+      var icon = this.parentNode.children[1];
+      pauseSong(audioFile, icon);
     });
   }
 };
@@ -15,13 +28,13 @@ var togglePlay = function(that) {
   if(that.classList.contains('fa-play')) {
     that.classList.remove('fa-play');
     that.classList.add('fa-stop');
-    audioFile = that.parentNode.children[1];
+    audioFile = that.parentNode.children[2];
     playSong(audioFile);
   }
   else {
     that.classList.remove('fa-stop');
     that.classList.add('fa-play');
-    audioFile = that.parentNode.children[1];
+    audioFile = that.parentNode.children[2];
     populateTitle(audioFile);
     stopSong(audioFile);
   }
@@ -32,14 +45,23 @@ var playSong = function(audioFile) {
   audioFile.play();
 };
 
+var pauseSong = function(audioFile, icon) {
+  title.innerHTML = 'Paused: <em>' + audioFile.dataset.title + '</em>';
+  icon.classList.remove('fa-stop');
+  icon.classList.add('fa-play');
+  audioFile.pause();
+
+};
+
 var stopSong = function(audioFile) {
-  title.innerHTML = 'Select a song!';
+  audioFile.currentTime = 0;
   audioFile.pause();
 };
 
 var populateTitle = function(audioFile) {
-  title.innerHTML = 'Now Playing: [' + audioFile.dataset.title + ']';
+  title.innerHTML = 'Now Playing: <em>' + audioFile.dataset.title + '</em>';
 };
 
 
-playOrStop();
+playOrStopAudio();
+pauseAudio();
